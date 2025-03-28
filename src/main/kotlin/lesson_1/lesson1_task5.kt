04bd1048f2f03c39f47d0851f1acfe835abe8ab5
TataPuH-90 <abzalov-rifat@mail.ru>
2025-03-28 18:50:39 +0300

    KS-1-5 time

---
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val timeOfFly = 13250
+    val hoursOfFly = "03"
+    val minutesOfFly = "40"
+    val secondsOfFly = "50"
+
+    print("$hoursOfFly:$minutesOfFly:$secondsOfFly")
+}
