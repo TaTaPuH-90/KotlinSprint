e65ea4e55151dbed69a1f48aeb990c7e4c98bbab
TataPuH-90 <abzalov-rifat@mail.ru>
2025-03-28 19:30:43 +0300

    KS-2-1 average score

---
+++ src/main/kotlin/lesson_2/lesson2_ task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_2
+
+fun main(){
+
+    val firstStudent = 3
+    val secondStudent = 4
+    val thirdStudent = 3
+    val fourthStudent = 5
+    val averageScore:Float = (firstStudent+secondStudent+thirdStudent+fourthStudent) / 4f
+
+    println(averageScore)
+
+
+}
