0fe53c93791e6e9067acedd4b7b96d4ba8288a0b
TataPuH-90 <abzalov-rifat@mail.ru>
2025-03-28 22:26:16 +0300

    KS-2-1 const val

--- src/main/kotlin/lesson_2/lesson2_ task1.kt
+++ src/main/kotlin/lesson_2/lesson2_ task1.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_2
 
+const val QUANTITY_STUDENT = 4
+
 fun main(){
 
-    val firstStudent = 3
+    val firstStudent:Float = 3f
     val secondStudent = 4
     val thirdStudent = 3
     val fourthStudent = 5
-    val averageScore:Float = (firstStudent+secondStudent+thirdStudent+fourthStudent) / 4f
+    val averageScore = (firstStudent+secondStudent+thirdStudent+fourthStudent) / QUANTITY_STUDENT
 
     println(averageScore)
-
-
 }
