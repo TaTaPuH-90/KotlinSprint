3ee45d91cb3fb73fabd42224f2886dac0da81f27
TataPuH-90 <abzalov-rifat@mail.ru>
2025-03-29 18:10:15 +0300

    KS-2-1 const val

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,11 +1,15 @@
 package org.example.lesson_1
 
+    const val SEC_HOURS = 3600
+    const val SEC_MIN = 60
+
 fun main() {
+    val given = 13250
+    val hours = given / SEC_HOURS
+    val minutes = (given % SEC_HOURS) / SEC_MIN
+    val seconds = given - (hours * SEC_HOURS) - (minutes * SEC_MIN)
 
-    val timeOfFly = 13250
-    val hoursOfFly = "03"
-    val minutesOfFly = "40"
-    val secondsOfFly = "50"
+    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 
-    print("$hoursOfFly:$minutesOfFly:$secondsOfFly")
+
 }
